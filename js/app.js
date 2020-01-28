c2424843fa5d5b614540eb2ddd5d84e8ba19db31
Middi <richard@richardmiddleton.me>
2020-01-28 10:35:57 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,43 @@
+const apiURL = "http://localhost:8888/api/movies?_format=json";
+
+new Vue({
+    el: '#app',
+
+    data: {
+        hello: 'Hello World, Again!',
+        names: [
+            {firstname: 'John', lastname:'Doe'},
+            {firstname: 'Jane', lastname:'Jones'},
+            {firstname: 'Will ', lastname:'Smith'}
+        ],
+        movies: {},
+        liveFilter: '',
+        genreFilter: '',
+        genres: []
+    },
+    ready: function(){
+        this.getMovies();
+    },
+    methods: {
+        getMovies: function() {
+            this.$http.get(apiURL).then(response => {
+                console.log(response.body)
+                const movies = response.body;
+                this.$set('movies', movies);
+
+                let genresArr=[];
+
+                movies.forEach(movie => {
+                    movie.field_genre.forEach(genre => {
+                        genresArr.push(genre.value)
+                    });
+                });
+                genresArr = [...new Set(genresArr)];
+                this.$set('genres', genresArr);
+
+              }, response => {
+                  console.log('something went wrong')
+              });
+        }
+    }
+})
