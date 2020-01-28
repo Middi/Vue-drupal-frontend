30e3446775b78b06cc0890a594395f88af6b53c6
Middi <richard@richardmiddleton.me>
2020-01-28 11:39:23 +0100

    removed old dummy data

--- js/app.js
+++ js/app.js
@@ -4,12 +4,6 @@ new Vue({
     el: '#app',
 
     data: {
-        hello: 'Hello World, Again!',
-        names: [
-            {firstname: 'John', lastname:'Doe'},
-            {firstname: 'Jane', lastname:'Jones'},
-            {firstname: 'Will ', lastname:'Smith'}
-        ],
         movies: {},
         liveFilter: '',
         genreFilter: '',
